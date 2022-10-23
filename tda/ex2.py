5d97becb190cc3365f3f432dae85beb2714f51e8
Michell Aristobolo de Mello <86683354+michellamello@users.noreply.github.com>
2022-10-23 11:48:29 -0300

    Add files via upload

---
+++ tda/ex2.py
@@ -0,0 +1,5 @@
+sal = float(input())
+
+novo = sal + sal * 25 / 100
+
+print(novo)
---
+++ tda/ex3.py
@@ -0,0 +1,8 @@
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+
+m = (n1 + n2 + n3) / 3
+
+print(m)
+
